86d1de4a9f40ca2c74b3817f24048053ab83d247
sankik82 <sankik.suman@vmatechlabs.com>
2024-02-01 13:24:50 +0530

    change background color in paragraph

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,7 +12,7 @@ const About = () => {
       <div className="aboutBottom"></div>
       <h1 className="bg-success">ABOUT US</h1>
       
-      <p className="p-5">
+      <p className="p-5 bg-success ">
         Welcome to our Tea Shop! We are a cozy and inviting tea house dedicated
         to providing a unique and memorable tea experience for our customers.
         Our tea shop offers a wide variety of high-quality teas sourced from
